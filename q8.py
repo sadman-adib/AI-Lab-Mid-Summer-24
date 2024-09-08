bbfbd3a4893ad5d2fa8072ab512914f827733525
Sadman H Adib <sadmanadib5355@gmail.com>
2024-09-09 01:48:44 +0600

    Update q8.py

--- q8.py
+++ q8.py
@@ -1,7 +1,8 @@
-import numpy as np
-
 L =[[1,6,7],
     [3,5],
     [1,3,2]]
 
-print(np.array(L))
+# Can you convert this 2-D list to a 2-D integer numpy array?
+
+# No, Because the length of all subarrays are not same.
+
